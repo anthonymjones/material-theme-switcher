fe934e57ceac577ebdfa0696c507740c4248e295
anthonymjones <anthonyjones0519@gmail.com>
2020-02-21 09:19:35 -0700

    added takeUntil to valueChanges subscripton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ThemeOption, UserPrefsService } from './user-prefs.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnDestroy, OnInit {
   userName = new FormControl('');
   submitted = false;
   toggleTheme = new FormControl(false);
+  destroy$ = new Subject();
 
   constructor(private userPrefsService: UserPrefsService) {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
     this.toggleTheme.patchValue(
       this.userPrefsService.preferredTheme === ThemeOption.DARK
     );
 
-    this.toggleTheme.valueChanges.subscribe((toggleValue: boolean) => {
-      this.userPrefsService.preferredTheme = toggleValue
-        ? ThemeOption.DARK
-        : ThemeOption.LIGHT;
-    });
+    this.toggleTheme.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((toggleValue: boolean) => {
+        this.userPrefsService.preferredTheme = toggleValue
+          ? ThemeOption.DARK
+          : ThemeOption.LIGHT;
+      });
   }
 
   clear(): void {
